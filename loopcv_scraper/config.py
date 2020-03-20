7fcffce0000575f7530e429c443f65756e1531f8
fotisskal <52207059+fotisskal@users.noreply.github.com>
2020-03-20 15:48:40 +0200

    First commit of linkedin scraper

---
+++ loopcv_scraper/config.py
@@ -0,0 +1,72 @@
+LINKEDIN_HOME_URL = 'https://www.linkedin.com'
+LINKEDIN_LOGIN_URL = 'https://www.linkedin.com/uas/login'
+LINKEDIN_SEARCH_URL = 'https://www.linkedin.com/search/results/all/?keywords=%(company)s%20%(location)s&origin=GLOBAL_SEARCH_HEADER&page=%(page)s'
+LINKEDIN_PEOPLE_URL = 'https://www.linkedin.com/company/%s/people/'
+LINKEDIN_PEOPLE_LOCATION_FILTER_URL = 'https://www.linkedin.com/company/%(company)s/people/?keywords=%(location)s'
+
+DRIVER_PATH = '/usr/local/bin/chromedriver'
+
+ELEMENT_LOAD_TIME = 6
+PAGE_LOAD_TIME = 5
+SCROLL_PAUSE_TIME = 4
+TELNET_RESPONSE_TIME = 4
+TELNET_DELAY_TIME = 5
+FILL_BOX_DELAY_TIME = 4
+
+CREDENTIALS = {
+    "email": "fotis.kal21@gmail.com",
+    "password": "javaman698"
+}
+
+MAIL_FORMATS = [
+    "%(first_name)s%(last_name)s",
+    "%(first_name)s.%(last_name)s",
+    "%(first_name)s_%(last_name)s",
+    "%(first_name)s-%(last_name)s",
+    "%(last_name)s%(first_name)s",
+    "%(last_name)s.%(first_name)s",
+    "%(last_name)s_%(first_name)s",
+    "%(last_name)s-%(first_name)s",
+    "%(first_name)s",
+    "%(last_name)s"
+]
+
+XPATHS = {
+    "login_username": '//*[@id="username"]',
+    "login_password": '//*[@id="password"]',
+    "login_button": '//*[@id="app__container"]/main/div/form/div[3]/button',
+    "login_button_v2": '//*[@id="app__container"]/main/div/form/div[4]/button',
+    "company_url": "/html/body/div[5]/div[4]/div[3]/div/div[3]/section/div/div/div[2]/div[1]/div[2]/div/div/a",
+    "company_url_v2": "/html/body/div[6]/div[4]/div[3]/div/div[3]/section/div/div/div[2]/div[1]/div[2]/div/div/a",
+    "employees_number": "/html/body/div[5]/div[4]/div[3]/div/div[3]/div/div[2]/div[1]/div[1]/span",
+    "employees_number_v2": "/html/body/div[6]/div[4]/div[3]/div/div[3]/div/div[2]/div[1]/div[1]/span",
+    "profile_name": "div/section/div/artdeco-entity-lockup/artdeco-entity-lockup-content/artdeco-entity-lockup-title/a/div",
+    "position": "div/section/div/artdeco-entity-lockup/artdeco-entity-lockup-content/artdeco-entity-lockup-subtitle/div/div",
+    "profile_url": "div/section/div/artdeco-entity-lockup/artdeco-entity-lockup-content/artdeco-entity-lockup-title/a",
+    "contact_info": "/html/body/div[4]/div/div/div[2]/section/div/div[1]/div/section[2]/div/a"
+}
+
+CLASSES = {
+    "company_people": "org-people-profiles-module__profile-list"
+}
+
+# Rating [1, 5]
+RECRUITMENT_KEYWORDS = {
+    "HR": 5,
+    "hr": 5,
+    "Human Resources": 5,
+    "human resources": 5,
+    "Director": 3,
+    "director": 3,
+    "Staff": 5,
+    "staff": 5,
+    "Staffing": 5,
+    "staffing": 5,
+    "Talent": 5,
+    "talent": 5,
+    "People": 4,
+    "people": 4
+}
+
+MONGO_HOST = 'localhost'
+MONGO_PORT = 27017
---
+++ loopcv_scraper/tests/test_linkedin_scraper.py
@@ -0,0 +1,78 @@
+from unittest import TestCase
+
+from linkedin_scraper import LinkedinScraper
+
+
+class TestLinkedinScraper(TestCase):
+
+    def setUp(self):
+        self.scraper = LinkedinScraper()
+
+
+class TestA(TestLinkedinScraper):
+    def test_get_mail_from_linkedin_profile(self):
+        self.scraper.linkedin_login()
+        mail = self.scraper.get_mail_from_linkedin_profile("https://www.linkedin.com/in/fotis-kalathopoulos-58719186")
+        print(mail)
+        self.assertEqual(mail, 'fotis.kal21@gmail.com')
+
+
+class TestB(TestLinkedinScraper):
+    def test_construct_mail_pool(self):
+        mail_list = self.scraper.construct_mail_pool('Fotis Kalathop', '@niometrics.com')
+        print(mail_list)
+        self.assertEqual(mail_list.__len__(), 400)
+
+
+class TestC(TestLinkedinScraper):
+    def test_find_valid_mail(self):
+        mail_list = ['katsoulis@navarino.gr']
+        valid_mail = self.scraper.find_valid_mail_format(mail_list, 'navarino-gr.mail.protection.outlook.com',
+                                                         'navarino.gr')
+        print(valid_mail)
+        # self.assertEqual(valid_mail, 'kalathopoulos@niometrics.com')
+
+
+class TestD(TestLinkedinScraper):
+    def test_linkedin_login(self):
+        self.scraper.linkedin_login()
+        self.scraper.get_company_info_and_people('niometrics', 'Greece')
+        print(self.scraper.driver.page_source)
+
+
+class TestE(TestLinkedinScraper):
+    def test_get_recruiters(self):
+        self.scraper.linkedin_login()
+        self.scraper.get_company_info_and_people('sg-digital-division-of-scientific-games-corporation', 'Greece')
+        print(self.scraper.get_recruiters_from_company_people('sg-digital', 'Greece'))
+
+
+class TestF(TestLinkedinScraper):
+    def test_get_company_url_and_people_number(self):
+        self.scraper.linkedin_login()
+        self.scraper.get_company_info_and_people('niometrics', 'Greece')
+        self.assertEqual(self.scraper.get_company_url(), 'https://www.niometrics.com/')
+        self.assertEqual(self.scraper.get_employees_number(), '36')
+
+
+class TestG(TestLinkedinScraper):
+    def test_get_company_people_list(self):
+        self.scraper.linkedin_login()
+        self.scraper.get_company_info_and_people('niometrics', 'Greece')
+        employee_list = self.scraper.get_employees_list()
+        employees = employee_list.find_elements_by_tag_name('li')
+        self.assertEqual(employees.__len__(), '36')
+
+
+class TestH(TestLinkedinScraper):
+    def test_create_company_name(self):
+        print(self.scraper.convert_company_name('sg-digital-division-of-scientific-games-corporation'))
+
+
+class TestI(TestLinkedinScraper):
+    def test_page_source(self):
+        self.scraper.linkedin_login()
+        self.scraper.get_company_info_and_people('niometrics', 'Greece')
+        html_source = self.scraper.driver.page_source
+        print(html_source)
+
